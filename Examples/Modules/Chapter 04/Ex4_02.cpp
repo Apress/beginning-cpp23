209e50ce0ced1d5153be1d9722b77df6dc7743d9
Peter Van Weert <peter.vanweert@gmail.com>
2022-08-21 17:32:15 +0200

    C++23: Chapter 4
    (cherry picked from commit df2ad12192158a438059e24adc02c4bf989644af)

--- Examples/Modules/Chapter 04/Ex4_02.cpp
+++ Examples/Modules/Chapter 04/Ex4_02.cpp
@@ -1,18 +1,16 @@
 // Three-way comparison of integers
 import <compare>;  // Required when using operator <=> (even for fundamental types)
-import <format>;
-import <iostream>;
+#include <print>
 
 int main()
 {
-  std::cout << "Please enter a number: ";
-
+  std::print("Please enter a number: ");
   int value;
   std::cin >> value;
 
   std::strong_ordering ordering{ value <=> 0 };
 
-  std::cout << std::format("value < 0: {}\n", ordering == std::strong_ordering::less);
-  std::cout << std::format("value > 0: {}\n", ordering == std::strong_ordering::greater);
-  std::cout << std::format("value == 0: {}\n", ordering == std::strong_ordering::equal);
+  std::println("value < 0: {}", ordering == std::strong_ordering::less);
+  std::println("value > 0: {}", ordering == std::strong_ordering::greater);
+  std::println("value == 0: {}", ordering == std::strong_ordering::equal);
 }
