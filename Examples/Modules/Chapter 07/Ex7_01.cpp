5cd74cb15c75c1f1b93d12762c3447e95548590c
Peter Van Weert <peter.vanweert@gmail.com>
2022-10-25 17:39:48 +0200

    C++23: Chapter 7 (add examples)

---
+++ Examples/Modules/Chapter 07/Ex7_01.cpp
@@ -0,0 +1,20 @@
+// Concatenating strings
+import std;
+
+int main()
+{
+  std::string first;                             // Stores the first name
+  std::string second;                            // Stores the second name
+
+  std::print("Enter your first name: ");
+  std::cin >> first;                             // Read first name
+
+  std::print("Enter your second name: ");
+  std::cin >> second;                            // Read second name
+
+  std::string sentence {"Your full name is "};   // Create basic sentence
+  sentence += first + " " + second + ".";        // Augment with names
+
+  std::println("{}", sentence);      // Print the sentence and its length
+  std::println("The string contains {} characters.", sentence.length());
+}
