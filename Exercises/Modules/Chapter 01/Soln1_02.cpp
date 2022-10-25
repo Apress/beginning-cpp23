a3c57ed3c56a3292bf328573cfa91f9035924dfe
Peter Van Weert <peter.vanweert@gmail.com>
2022-10-25 17:37:59 +0200

    C++23: Chapter 1 exercises

--- Exercises/Modules/Chapter 01/Soln1_02.cpp
+++ Exercises/Modules/Chapter 01/Soln1_02.cpp
@@ -1,11 +1,12 @@
 // Exercise 1-2. Write your name and age on successive lines.
-// There are several possibilities. You can do it in one statement for example.
-// You could also output '\n' to go to a new line.
+// There are several possibilities. You can do it in one statement for example,
+// using '\n' to go to a new line.
 
 import std;
 
 int main()
 {
-  std::cout << "Phil McCavity" << std::endl;       // Name
-  std::cout << "Age: 88" << std::endl;             // and age
+  int age{ 88 };
+  std::println("Phil McCavity");       // Name
+  std::println("Age: {}", age);        // and age
 }
