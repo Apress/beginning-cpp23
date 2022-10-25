04fe9fae43802bb9fb76c55aaa522ca59caee1dd
Peter Van Weert <peter.vanweert@gmail.com>
2022-10-25 17:37:06 +0200

    C++23: Chapter 9 examples

--- Examples/Modules/Chapter 09/Ex9_01.cpp
+++ Examples/Modules/Chapter 09/Ex9_01.cpp
@@ -1,39 +1,40 @@
 // Working with std::optional<>
 import std;
 
-std::optional<size_t> find_last(
+std::optional<std::size_t> find_last(
    const std::string& string, char to_find,
-   std::optional<size_t> start_index = std::nullopt); // or: ... start_index = {});
+   std::optional<std::size_t> start_index = std::nullopt); // or: … start_index = {});
 
 int main()
 {
-  const auto string{ "Growing old is mandatory; growing up is optional." };
+  const auto string = "Growing old is mandatory; growing up is optional.";
 
-  const std::optional<size_t> found_a{ find_last(string, 'a') };
+  const std::optional<std::size_t> found_a{ find_last(string, 'a') };
   if (found_a)
-    std::cout << "Found the last a at index " << *found_a << std::endl;
+    std::println("Found the last a at index {}", *found_a);
 
   const auto found_b{ find_last(string, 'b') };
   if (found_b.has_value())
-    std::cout << "Found the last b at index " << found_b.value() << std::endl;
+    std::println("Found the last b at index {}", found_b.value());
 
-// following line gives an error (cannot convert std::optional<size_t> to size_t)
-// const size_t found_c{ find_last(string, 'c') }; 
+// Following line gives an error (cannot convert optional<size_t> to size_t)
+// const std::size_t found_c{ find_last(string, 'c') }; 
                                                    
   const auto found_early_i{ find_last(string, 'i', 10) };
   if (found_early_i != std::nullopt)
-    std::cout << "Found an early i at index " << *found_early_i << std::endl;
+    std::println("Found an early i at index {}", *found_early_i);
 }
 
-std::optional<size_t> find_last(const std::string& string, char to_find, 
-                                std::optional<size_t> start_index)
+std::optional<std::size_t> find_last(
+  const std::string& string, char to_find, std::optional<std::size_t> start_index
+)
 {
   // code below will not work for empty strings  
   if (string.empty())            
-    return std::nullopt;         // or: 'return std::optional<size_t>{};'
-                                 // or: 'return {};'
+    return std::nullopt;     // or: 'return std::optional<std::size_t>{};'
+                             // or: 'return {};'
   // determine the starting index for the loop that follows:
-  size_t index{ start_index.value_or(string.size() - 1) };
+  std::size_t index = start_index.value_or(string.size() - 1);
 
   while (true)  // never use while (index >= 0) here, as size_t is always >= 0!
   {
--- Examples/Modules/Chapter 09/Ex9_03A.cpp
+++ Examples/Modules/Chapter 09/Ex9_03A.cpp
@@ -21,11 +21,11 @@ int main()
   const std::array array_data{ 3.5, 5.0, 6.0, -1.2, 8.7, 6.4 }; // Throwing in an std::array for good measure
   const std::vector<std::string> names {"Charles Dickens", "Emily Bronte", 
                                   "Jane Austen", "Henry James", "Arthur Miller"};
-  std::cout << "The largest of array is " << largest(array) << std::endl;
-  std::cout << "The largest of numbers is " << largest(numbers) << std::endl;
-  std::cout << "The largest of data is " << largest(data) << std::endl;
-  std::cout << "The largest of array_data is (also) " << largest(array_data) << std::endl;
-  std::cout << "The largest of names is " << largest(names) << std::endl;
+  std::println("The largest of array is {}", largest(array));
+  std::println("The largest of numbers is {}", largest(numbers));
+  std::println("The largest of data is {}", largest(data));
+  std::println("The largest of array_data is (also) {}", largest(array_data));
+  std::println("The largest of names is {}", largest(names));
 }
 
 // Finds the largest of a span of values
