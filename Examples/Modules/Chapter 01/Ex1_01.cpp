9e27539e616b6806dbce91c729a273499940cce1
Peter Van Weert <peter.vanweert@gmail.com>
2022-12-28 16:26:38 +0100

    Chapter 1: improve comments
    (cherry picked from commit 725942131ac77daa54eea4e98311ade30734f10f)

--- Examples/Modules/Chapter 01/Ex1_01.cpp
+++ Examples/Modules/Chapter 01/Ex1_01.cpp
@@ -1,11 +1,9 @@
 // A complete C++ program
-import std;
-
-int main()
+import std;          // This line makes the entire Standard Library available,
+                     // including the std::println() functionality used below
+int main() 
 {
-  int answer {42};              // Defines answer with value 42
-
-  std::println("The answer to life, the universe, and everything is {}", answer);
-
+  int answer {42};   // Defines the variable answer with value 42
+  std::println("The answer to life, the universe, and everything is {}.", answer);
   return 0;
 }
