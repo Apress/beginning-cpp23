c20fe4b901df8cf86024c437faac51edc1f88cf7
Peter Van Weert <peter.vanweert@gmail.com>
2022-12-28 16:25:19 +0100

    Chapter 2: examples revised

--- Examples/Modules/Chapter 02/Ex2_03.cpp
+++ Examples/Modules/Chapter 02/Ex2_03.cpp
@@ -1,28 +1,18 @@
-// Sizing a pond for happy fish
+// Finding maximum and minimum values for data types
 import std;
 
 int main()
 {
-  // 2 square feet pond surface for every 6 inches of fish
-  const double fish_factor { 2.0/0.5 };  // Area per unit length of fish
-  const double inches_per_foot { 12.0 };
-
-  double fish_count {};            // Number of fish
-  double fish_length {};           // Average length of fish
-
-  std::print("Enter the number of fish you want to keep: ");
-  std::cin >> fish_count;
-  std::print("Enter the average fish length in inches: ");
-  std::cin >> fish_length;
-  fish_length /= inches_per_foot;  // Convert to feet
-  std::println("");
-
-  // Calculate the required surface area
-  const double pond_area {fish_count * fish_length * fish_factor};
-
-  // Calculate the pond diameter from the area
-  const double pond_diameter {2.0 * std::sqrt(pond_area / std::numbers::pi)};
-
-  std::println("Pond diameter required for {} fish is {} feet.", 
-               fish_count, pond_diameter);
+  std::println("The range for type short is from {} to {}",
+    std::numeric_limits<short>::min(), std::numeric_limits<short>::max());
+  std::println("The range for type unsigned int is from {} to {}",
+    std::numeric_limits<unsigned int>::min(), std::numeric_limits<unsigned int>::max());
+  std::println("The range for type long long is from {} to {}",
+    std::numeric_limits<long long>::min(), std::numeric_limits<long long>::max());
+  std::println("The positive range for type float is from {} to {}",
+    std::numeric_limits<float>::min(), std::numeric_limits<float>::max());
+  std::println("The full range for type float is from {} to {}",
+    std::numeric_limits<float>::lowest(), std::numeric_limits<float>::max());
+  std::println("The positive range for type double is from {} to {}",
+    std::numeric_limits<double>::min(), std::numeric_limits<double>::max());
 }
