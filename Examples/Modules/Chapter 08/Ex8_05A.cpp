c9a2d90892e37a144229a6a21c05ff5476f7d7bb
Peter Van Weert <peter.vanweert@gmail.com>
2022-12-28 16:27:47 +0100

    Chapter 8: use triplet instead of values

--- Examples/Modules/Chapter 08/Ex8_05A.cpp
+++ Examples/Modules/Chapter 08/Ex8_05A.cpp
@@ -8,8 +8,8 @@ double average10(double array[10]);        // Function prototype
 int main()
 {
  // double values[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
-  double values[] { 1.0, 2.0, 3.0 };           // Only three values!!!
-  std::println("Average = {}", average10(values));
+  double triplet[] { 1.0, 2.0, 3.0 };      // Only three values!!!
+  std::println("Average = {}", average10(triplet));
 }
 
 // Function to compute an average
