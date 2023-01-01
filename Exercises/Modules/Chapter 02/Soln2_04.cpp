3807c1691e55a439e4e5bf80b9228a7d969749dc
Peter Van Weert <peter.vanweert@gmail.com>
2023-01-01 13:57:17 +0100

    Chapter 2: new exercise 2 + renumber others after adding new exercise 1

--- Exercises/Modules/Chapter 02/Soln2_04.cpp
+++ Exercises/Modules/Chapter 02/Soln2_04.cpp
@@ -1,26 +1,35 @@
-// Exercise 2-4. Calculating the Body Mass Index.
-// In the expression for bmi, both h_feet and h_inches will be implicitly converted to double
-// because the other operand of the sub-expression is double.
-
+// Exercise 2-3. Calculating the height of a tree
 import std;
 
 int main()
 {
-  const double lbs_per_kg {2.2};
   const double inches_per_foot {12.0};
-  const double meters_per_foot {0.3048};
+  const double pi_degrees {180.0};
+  double feet {};
+  double inches {};
+
+  std::cout << "Enter the distance from the tree in feet and inches: ";
+  std::cin >> feet >> inches;
+  const double distance {feet + inches / inches_per_foot};
+
+  double angle {};
+  std::cout << "Enter the angle of the top of the tree in degrees: ";
+  std::cin >> angle;
   
-  double w_lbs {};
-  unsigned int h_feet {};
-  unsigned int h_inches {};
+  // First convert angle to radians 
+  // (the trigoniometric functions of <cmath> operate with radians)
+  angle *= std::numbers::pi / pi_degrees;     
+                                              
+  double eye_height {};
+  std::cout << "Enter your eye height from the ground in inches: ";
+  std::cin >> eye_height;
+  eye_height /= inches_per_foot;                                  // Convert to feet
+  
+  const double height {eye_height + distance * std::tan(angle)};  // Tree height in feet
+  const unsigned height_feet {static_cast<unsigned>(height)};
+  const unsigned height_inches {static_cast<unsigned>(std::round(inches_per_foot * (height - height_feet)))};
 
-  std::cout << "Enter your weight in pounds: ";
-  std::cin >> w_lbs;
-  std::cout << "Enter you height in feet and inches: ";
-  std::cin >> h_feet >> h_inches;
-
-  const double w_kg {w_lbs / lbs_per_kg};
-  const double h_meters { meters_per_foot * (h_feet + h_inches / inches_per_foot) };
-  const double bmi {w_kg / (h_meters * h_meters)};
-  std::cout << "Your BMI is " << bmi << std::endl;
+  std::cout << "\nThe height of the tree is "
+            << height_feet << " feet "
+            << height_inches << " inches.\n" << std::endl;
 }
