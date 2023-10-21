b7b4d8317b9ccad3622564dbfd9dd7951cd03fb4
Peter Van Weert <peter.vanweert@gmail.com>
2023-10-21 14:20:30 +0200

    Use import std;

--- Examples/Appendix A/ExA_12/Hypot.cpp
+++ Examples/Appendix A/ExA_12/Hypot.cpp
@@ -1,7 +1,7 @@
 // Hypot.cpp - Definition of the math::hypot() function
 #include "ProperMath.h"  // For the square() function template definition
 #include "Hypot.h"       // Missing from the code listing in the book!
-#include <cmath>         // For std::sqrt()
+import std;              // For std::sqrt()
 
 // Caution: See Chapter 11 for why you should always use std::hypot() 
 // over this naïve definition (based on the Pythagorean Theorem)!
